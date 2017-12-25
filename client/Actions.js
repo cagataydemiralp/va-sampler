58cb7593652702f4f0f6914821ce0ce5daf243f3
Çağatay Demiralp <cagatay.demiralp@us.ibm.com>
2017-12-25 13:29:58 -0500

    Remove compute server api

--- client/Actions.js
+++ client/Actions.js
@@ -1,15 +1,11 @@
 import axios from 'axios'; 
 import * as types  from './ActionTypes'; 
 
-// const api = '//35.196.240.209/api/sample/';
-// use
 const api = '//127.0.0.1:5000/api/sample/';  
-// when you run the serve locally  
 
 export function sampleUpdate(data, name, format) {
 
   const actionName = name.toUpperCase() + '_' + format.toUpperCase() + '_UPDATE',  
-
   type = types[actionName]; 
 
   return{ type, data };  
